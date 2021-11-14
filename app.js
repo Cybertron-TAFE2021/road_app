030c62c162a18db4b4a4d7653f0a888e4b97ad47
jethro revalde <jethro.revalde@hotmail.com>
2021-11-14 23:35:58 +1100

    getting warmer

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 
+const path = require("path"); //I have no idea what this is for but it's a core module apparently.
+const logger = require("morgan");
+
 const https = require("https");
 
 const app = express();
 
+const _ = require("lodash");
+
+const neo4j = require("neo4j-driver"); //The MOST IMPORTANT BIT OF THIS APP.
+
+const driver =  neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j"), "TAFE2021");
+
+var session = driver.session(); //create a session variable.
+
 app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+
 
 
 
